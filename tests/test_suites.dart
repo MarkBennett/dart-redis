ba644f73928080f678ee19ae49f5882aa9b80623
Mark Bennett <mark@burmis.ca>
2012-04-24 20:57:20 -0600

    Futures and callbacks working.
    I need to create the Redis protocol encoder and de-coder next.

--- tests/test_suites.dart
+++ tests/test_suites.dart
@@ -5,6 +5,37 @@
 
 testSuites() {
 	describe("Redis.connect", () {
-		it("should return a connection", () => expect(Redis.connect("localhost")).to(not(beNull())));
+		RedisConnection conn;
+
+		beforeEach(() => conn = Redis.connect("localhost"));
+
+		it("should return a connection", () => expect(conn).to(not(beNull())));
+
+		it("should be connecting to the right hostname", () => expect(conn.hostname).to(equal("localhost")));
+	});
+
+	describe("RedisConnection", () {
+		RedisConnection conn;
+
+		beforeEach(() => conn = new RedisConnection("localhost"));
+
+		it('should be connected', () => expect(conn.connected).to(beTrue()));
+
+		describe("after disconnecting", () {
+			beforeEach(() => conn.close());
+
+			it('should not be connected', () => expect(conn.connected).to(beFalse()));
+		});
+		
+		describe("setting a key", () {
+			beforeEach(() => conn.set("test", "value1"));
+
+			it("should set the key in redis", () {
+			  var ret_val;
+			  
+			  conn.get("test", (val) => ret_val = val);
+	      expect(ret_val).to(equal("value1"));			  
+			});
+		});
 	});
 }
